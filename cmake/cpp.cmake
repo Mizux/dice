a36ef2531c9d9a4bd08ada727d4be6f03231130b
Corentin Le Molgat <corentinl@google.com>
2025-03-21 17:26:45 +0100

    cleanup

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -35,23 +35,22 @@ add_subdirectory(libdice)
 add_subdirectory(diceapp)
 
 # Install
-include(GNUInstallDirs)
-install(EXPORT DiceTargets
+install(EXPORT ${PROJECT_NAME}Targets
   NAMESPACE ${PROJECT_NAMESPACE}::
-  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Dice
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
   COMPONENT Devel)
 include(CMakePackageConfigHelpers)
-configure_package_config_file(cmake/DiceConfig.cmake.in
-  "${PROJECT_BINARY_DIR}/DiceConfig.cmake"
-  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/Dice"
+configure_package_config_file(cmake/${PROJECT_NAME}Config.cmake.in
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
   NO_SET_AND_CHECK_MACRO
   NO_CHECK_REQUIRED_COMPONENTS_MACRO)
 write_basic_package_version_file(
-  "${PROJECT_BINARY_DIR}/DiceConfigVersion.cmake"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
   COMPATIBILITY SameMajorVersion)
 install(
   FILES
-  "${PROJECT_BINARY_DIR}/DiceConfig.cmake"
-  "${PROJECT_BINARY_DIR}/DiceConfigVersion.cmake"
-  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/Dice"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
   COMPONENT Devel)
